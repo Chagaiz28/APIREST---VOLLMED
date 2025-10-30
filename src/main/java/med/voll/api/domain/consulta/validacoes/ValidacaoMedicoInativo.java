00174097224590d37d5a9914f8ed0e8a4c7914e9
Henrique Pereira Chagas Corrêa <henrique.correa@inatel.br>
2025-10-30 09:08:25 -0300

    Feat: Validacao medico ativo

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoMedicoInativo.java
@@ -0,0 +1,17 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.medico.MedicoRepository;
+
+public class ValidacaoMedicoInativo {
+
+    private MedicoRepository medicoRepository;
+
+    public voidvalidar(DadosAgendamentoConsulta dados){
+        if(dados.idMedico() == null){
+            return;
+        }
+
+        var medicoEstaAtivo = medicoRepository.findAtivoById(dados.idMedico());
+    }
+}
